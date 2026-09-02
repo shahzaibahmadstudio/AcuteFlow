0c5a2f2d74001b49c281167845b47fe354c68a6a
Shahzaib Ahmad <shahzaibahmad.studio@gmail.com>
2026-09-02 18:17:09 +0500

    feat(main.dart, splash.yaml): Implementing native splash screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
-void main() {
+void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const AcuteFlowApp());
 }
 
-class AcuteFlowApp extends StatelessWidget {
+class AcuteFlowApp extends StatefulWidget {
   const AcuteFlowApp({super.key});
+
+  @override
+  State<AcuteFlowApp> createState() => _AcuteFlowAppState();
+}
+
+class _AcuteFlowAppState extends State<AcuteFlowApp> {
+
+  @override
+  void initState() {
+    super.initState();
+    splashInitialization();
+  }
+
+  void splashInitialization() async {
+    await Future.delayed(const Duration(seconds: 2));
+    FlutterNativeSplash.remove();
+  }
 
   @override
   Widget build(BuildContext context) {
